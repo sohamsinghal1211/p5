2dedcf12216a7189887d9ef8dda8021325ace5f0
sohamsinghal1211 <80926575+sohamsinghal1211@users.noreply.github.com>
2022-02-27 18:46:10 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(640, 480);
+    canvas.position(150, 150);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    tint_color="";
+}
+
+function draw(){
+    image(video,230 ,150 ,220 , 200);
+    tint(tint_color)
+    fill("green")
+    rect(90, 40, 460, 20);
+    rect(40, 90, 20, 300);
+    rect(90, 420, 460, 20);
+    rect(580, 90, 20, 300);
+    fill("red")
+    circle(50, 50, 80)
+    circle(50, 430, 80)
+    circle(590, 50, 80)
+    circle(590, 430, 80)
+}
+
+
+
+function take_snapshot(){
+    save('filtercolor.png');
+}
